384cfd9346873f8101f3a106e857742178a9d2fd
ZzzM <zzzm.github@outlook.com>
2024-07-16 00:11:48 +0800

    Update

---
+++ config/sing-box/template/advanced.js
@@ -0,0 +1,57 @@
+const { type, name, port } = $arguments
+const compatible_outbound = {
+  tag: 'COMPATIBLE',
+  type: 'REJECT',
+}
+
+let compatible
+let config = JSON.parse($files[0])
+let proxies = await produceArtifact({
+  name,
+  type: /^1$|col/i.test(type) ? 'collection' : 'subscription',
+  platform: 'sing-box',
+  produceType: 'internal',
+})
+
+config.inbounds[0].platform.http_PROXY.server_port = port
+
+config.inbounds[1].listen_port = port
+
+config.outbounds.push(...proxies)
+
+config.outbounds.map(i => {
+  if (['Automatic', 'Manual'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies))
+  }
+  if (['HK'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /港|hk|hongkong|kong kong|🇭🇰/i))
+  }
+  if (['JP'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /日本|jp|japan|🇯🇵/i))
+  }
+  if (['SG'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /^(?!.*(?:us)).*(新|sg|singapore|🇸🇬)/i))
+  }
+  if (['US'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /美|us|unitedstates|united states|🇺🇸/i))
+  }
+  if (['KR'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /韩/i))
+  }
+})
+
+config.outbounds.forEach(outbound => {
+  if (Array.isArray(outbound.outbounds) && outbound.outbounds.length === 0) {
+    if (!compatible) {
+      config.outbounds.push(compatible_outbound)
+      compatible = true
+    }
+    outbound.outbounds.push(compatible_outbound.tag);
+  }
+});
+
+$content = JSON.stringify(config, null, 2)
+
+function getTags(proxies, regex) {
+  return (regex ? proxies.filter(p => regex.test(p.tag)) : proxies).map(p => p.tag)
+}
---
+++ config/sing-box/template/simple.js
@@ -0,0 +1,42 @@
+const { type, name, port } = $arguments
+const compatible_outbound = {
+  tag: 'COMPATIBLE',
+  type: 'REJECT',
+}
+
+let compatible
+let config = JSON.parse($files[0])
+let proxies = await produceArtifact({
+  name,
+  type: /^1$|col/i.test(type) ? 'collection' : 'subscription',
+  platform: 'sing-box',
+  produceType: 'internal',
+})
+
+config.inbounds[0].platform.http_PROXY.server_port = port
+
+config.inbounds[1].listen_port = port
+
+config.outbounds.push(...proxies)
+
+config.outbounds.map(i => {
+  if (['Automatic', 'Manual'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies))
+  }
+})
+
+config.outbounds.forEach(outbound => {
+  if (Array.isArray(outbound.outbounds) && outbound.outbounds.length === 0) {
+    if (!compatible) {
+      config.outbounds.push(compatible_outbound)
+      compatible = true
+    }
+    outbound.outbounds.push(compatible_outbound.tag);
+  }
+});
+
+$content = JSON.stringify(config, null, 2)
+
+function getTags(proxies, regex) {
+  return (regex ? proxies.filter(p => regex.test(p.tag)) : proxies).map(p => p.tag)
+}
