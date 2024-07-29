2698dd3c56e572cf9bd99d3000e2962c409b4c5c
ZzzM <zzzm.github@outlook.com>
2024-07-29 20:30:08 +0800

    Update

--- config/mihomo/template/simple.js
+++ config/mihomo/template/simple.js
@@ -1,7 +1,7 @@
 const { type, name, port } = $arguments
 
 const sub = /^1$|col/i.test(type) ? 'collection' : 'subscription'
-const url = 'https://sub.store/download/$sub/$name?target=ClashMeta'
+const url = 'https://sub.store/download/${sub}/${name}?target=ClashMeta'
 
 $content = $content.replace(`$url`, url)
 $content = $content.replace(`$port`, port)
